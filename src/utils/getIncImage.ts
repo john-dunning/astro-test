355356bde851884b60c0f6a68ce63dc47445c750
John Dunning <john_dunning@yahoo.com>
2024-02-04 01:23:25 -0800

    Show image named with command slug at top of about page
    Fix incorrectly formatted release history sections in some .mxi files.
    Add getIncImage.ts to find images by name.
    Rename some .gif files in /inc to .png.

---
+++ src/utils/getIncImage.ts
@@ -0,0 +1,21 @@
+import type { ImageMetadata } from "astro";
+
+	// this Vite import method requires that its parameter be a literal string,
+	// not even one assembled from other literals
+const images = import.meta.glob<{ default: ImageMetadata }>("/src/assets/inc/*.{jpeg,jpg,png,gif}");
+
+export async function getIncImage(
+	filename: string = ""): Promise<ImageMetadata | null>
+{
+	if (filename) {
+		const imagePath = "/src/assets/inc/" + filename;
+
+		if (images[imagePath]) {
+			return (await images[imagePath]()).default;
+		}
+
+		console.error(`Missing image: ${imagePath}`);
+	}
+
+	return null;
+}
